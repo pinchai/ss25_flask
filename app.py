3e9d29019c8e7948d9519f7ac2ed3ea5ce94a932
Pinchai <>
2024-03-06 20:36:51 +0700

    config template

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template
-import requests
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
+    module = 'master'
+    return render_template('master.html', module=module)
+
+
+@app.route('/dashboard')
+def dashboard():
+    module = 'dashboard'
+    return render_template('dashboard.html', module=module)
+
+
+@app.route('/user')
+def user():
+    module = 'user'
+    a = [1]
+    print(a[10])
+
+    return render_template('user.html', module=module)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
     return render_template('404.html')
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html')
 
 
 if __name__ == '__main__':
